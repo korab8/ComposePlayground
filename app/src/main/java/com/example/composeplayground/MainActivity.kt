836f4ea01b401823c22fc45456c07ccca12fa7cd
Korab Muhadri <korab.muhadri@ext.pixida.de>
2023-01-31 16:21:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.composeplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize().scrollable(rememberScrollState(), Orientation.Vertical),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!, Hello $name!, Hello $name!, Hello $name!, Hello $name!, Hello $name!, Hello $name!, ")
+}
+
+@Preview(showBackground = true, widthDp = 200, heightDp = 75)
+@Composable
+fun DefaultPreview() {
+    ComposePlaygroundTheme {
+        Greeting("Android")
+    }
+}
